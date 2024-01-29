6cf93d9991750439b8b7c46ac68b4aa6439fc9bc
Sean Connolly <sean.connolly@northwestern.edu>
2024-01-29 09:35:33 +0000

    Loops Code Along

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -7,5 +7,38 @@
 #   puts "tacos!"
 # end
 
-# Loop through tacos
 # tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+# puts "#{tacos[0]}"
+# puts "#{tacos[1]}"
+# puts "#{tacos[2]}"
+
+# Loop through tacos
+
+tacos = ["carnitas", "carne asada", "pollo", "pescado", "veggie"]
+
+# Set the index to 0
+
+index = 0
+
+# Start the loop
+
+loop do
+
+# if the index is outside the range of the array, stop looping
+
+    if index == tacos.length
+        break
+    end
+
+# Put the taco at the index in memory
+
+    taco = tacos[index]
+
+# Write it to the screen
+
+    puts "#{tacos} tacos"
+
+# Increment the index
+
+    index = index + 1
+end
