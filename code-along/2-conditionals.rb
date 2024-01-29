efb0cfc127d1558352ed986d5d471289849e7fc3
Sean Connolly <sean.connolly@northwestern.edu>
2024-01-29 05:20:22 +0000

    Code Along Conditionals Done

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -2,7 +2,36 @@
 # is the same as where this file is located and then run:
 # ruby 2-conditionals.rb
 
+# Conditionals are decisions that you are asking the computer to make
+# If certain conditions are met a decision is made based on the code and we are given an output
+
+# A double equal sign "==" is a judgement
+# A single equal sign "=" is an assignment in the computer's memory
+
 # Booleans
+# if 1 + 1 == 2
+#     puts "This is true"
+# end
+
+# if 1 + 1 == 3
+#     puts "You should never see this"
+# end
+
+# Example
+
+# # Input
+# user_entered_password = "kale"
+# real_password = "tacos"
+
+# # Algorithm
+# if user_entered_password == real_password
+#     # Output
+#     puts "You're in."
+# else 
+#     puts "Sorry, wrong password."
+# end
+
+# =================================================
 
 # Boolean Expressions
 
@@ -12,4 +41,18 @@
 
 # Elsif Conditional Logic
 
+# Input
+my_team_score = 4
+opponent_team_score = 5
+
+if my_team_score > opponent_team_score
+    puts "We win!"
+elsif my_team_score == opponent_team_score
+    puts "We tied!"
+else
+    puts "We lost!"
+end
+
+
 # Combining Expressions
+
