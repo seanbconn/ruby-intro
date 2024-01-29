8e89fc7708960338fb261cef83a52a91139d038a
Sean Connolly <sean.connolly@northwestern.edu>
2024-01-29 04:43:51 +0000

    Completed Labs 1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -38,3 +38,26 @@
 # Nothing
 
 # puts nil
+
+# Store the number 5 in memory
+# x = 5
+
+# Store the number 2 in memory
+# y = 2
+
+# Multiply the two numbers and show them on screen
+# puts x * y
+
+# Store the number of tacos
+# number_of_tacos = 5
+
+# # Store my favorite food in memory
+# favorite_food = "Tacos"
+
+# # Write my food order to the screen (called concatonate line 51 and 54 together)
+# # This output with lines 51-52 and 54-55 will display "5 Tacos"
+# puts "#{number_of_tacos} #{favorite_food}"
+
+# # Fun things
+# # puts code.upcase makes all the output text ALL CAPS
+# # puts code.reverse makes all the output in reverse order 
--- labs/1-data.rb
+++ labs/1-data.rb
@@ -10,6 +10,18 @@
 # then write them out to the screen. Show the total
 # as well.
 
+# Create first random number and store it in memory
+# Create a second random number and store it in memory
+# Add the two random numbers and store that sum in memory
+
+x = rand(1..6)
+y = rand(1..6)
+z = x + y
+puts "Let's play craps!"
+puts "Die A="   "#{x}"
+puts "Die B="   "#{y}"
+puts "Die Sum="   "#{z}"
+
 # HINTS
 # Get a random number between 1 and 6 by doing:
 # rand(1..6)
